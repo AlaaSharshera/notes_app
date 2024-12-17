7192233248311967adf8b621f70853eb02370acc
alaa <alaasharshera123@gmail.com>
2024-12-17 20:32:36 +0200

    notes view custom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/notes_view.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -11,6 +13,7 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: NotesView(),
     );
