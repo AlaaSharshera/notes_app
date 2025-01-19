ec2253a5aae95e69ed6ecb9f4cb6887aff9853df
alaa <alaasharshera123@gmail.com>
2025-01-19 10:38:06 +0200

    add notes cubit

--- lib/cubits/addnote_cubit.dart/addnote_cubit.dart
+++ lib/cubits/addnote_cubit.dart/addnote_cubit.dart
@@ -1,6 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constant.dart';
+
 import 'package:notes_app/cubits/addnote_cubit.dart/addnote_states.dart';
+import 'package:notes_app/models/notes_model.dart';
 
 class AddnoteCubit extends Cubit<AddnoteCubitState> {
   AddnoteCubit() : super(AddNoteCubitInitialState());
+
+  addNote(NotesModel note) async {
+    emit(AddNoteCubitLoadingState());
+    try {
+      var notesBox = Hive.box<NotesModel>(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteCubitSuccessState());
+    } on Exception catch (e) {
+      emit(AddNoteCubitFailureState(e.toString()));
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constant.dart';
-import 'package:notes_app/cubits/add_note_cubit.dart/add_note_cubit.dart';
+import 'package:notes_app/cubits/addnote_cubit.dart/addnote_cubit.dart';
+
 import 'package:notes_app/models/notes_model.dart';
 import 'package:notes_app/views/notes_view.dart';
 
@@ -21,7 +22,7 @@ class NotesApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) {
-          return AddNoteCubit();
+          return AddnoteCubit();
         })
       ],
       child: MaterialApp(
